a333e6f7b2d0fa1543c780bc001a6042bbd09d09
James Goppert <james.goppert@gmail.com>
2014-06-05 23:17:20 -0400

    Added parsimonious dep.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='modpeg',
+    install_requires=['parsimonious'],
     tests_require=['nose'],
     test_suite='nose.collector',
     packages=find_packages(exclude="*._test"),
